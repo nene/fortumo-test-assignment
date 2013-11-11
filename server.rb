1849999b85865a5a9b89730524a5c850ff4bfa3e
Rene Saarsoo <nene@triin.net>
2013-11-11 14:19:43 +0200

    Clean up the server script.

--- server.rb
+++ server.rb
@@ -1,13 +1,3 @@
-# require 'webrick'
-# include WEBrick
-
-# port = 2000
-# puts "Starting server: http://#{Socket.gethostname}:#{port}"
-# server = HTTPServer.new(:Port=>2000,:DocumentRoot=>Dir::pwd )
-# trap("INT"){ server.shutdown }
-# server.start
-
-
 require 'socket' # Provides TCPServer and TCPSocket classes
 
 # Initialize a TCPServer object that will listen
